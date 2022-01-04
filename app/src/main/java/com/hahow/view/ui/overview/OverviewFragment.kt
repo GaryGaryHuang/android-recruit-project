00191b0a92344f162e6002e00065adca8d43e415
Gary_Huang <s096601@gmail.com>
2022-01-05 00:09:10 +0800

    [Feature] Course overview view (Not Completed)

--- app/src/main/java/com/hahow/view/ui/overview/OverviewFragment.kt
+++ app/src/main/java/com/hahow/view/ui/overview/OverviewFragment.kt
@@ -6,15 +6,52 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.hahow.android_recruit_project.databinding.FragmentOverviewBinding
+import com.hahow.view.adapter.CourseAdapter
 
 class OverviewFragment : Fragment() {
     private lateinit var binding: FragmentOverviewBinding
     private lateinit var overviewViewModel: OverviewViewModel
+    private lateinit var mCourseAdapter: CourseAdapter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentOverviewBinding.inflate(inflater, container, false)
         overviewViewModel = ViewModelProvider(this)[OverviewViewModel::class.java]
+        setListeners()
+        initValues()
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        overviewViewModel.isLoading.observe(viewLifecycleOwner, {
+            binding.swipeLayout.isRefreshing = it
+        })
+
+        overviewViewModel.message.observe(viewLifecycleOwner, {
+            Snackbar.make(binding.root, it, Snackbar.LENGTH_SHORT).show()
+        })
+
+        overviewViewModel.courseList.observe(viewLifecycleOwner, {
+            mCourseAdapter.submitList(it)
+        })
+    }
+
+    private fun setListeners() {
+        binding.swipeLayout.setOnRefreshListener {
+            overviewViewModel.getCourse()
+        }
+    }
+
+    private fun initValues() {
+        mCourseAdapter = CourseAdapter()
+        binding.rv.also {
+            it.layoutManager = LinearLayoutManager(requireContext(), RecyclerView.VERTICAL, false)
+            it.adapter = mCourseAdapter
+        }
     }
 }
--- app/src/main/java/com/hahow/view/ui/overview/OverviewViewModel.kt
+++ app/src/main/java/com/hahow/view/ui/overview/OverviewViewModel.kt
@@ -1,7 +1,27 @@
 package com.hahow.view.ui.overview
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hahow.domain.Course
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class OverviewViewModel : ViewModel() {
+    private val _courseList: MutableLiveData<List<Course>> = MutableLiveData(mutableListOf())
+    val courseList: LiveData<List<Course>> = _courseList
+    private val _message: MutableLiveData<String> = MutableLiveData()
+    val message: LiveData<String> = _message
+    private val _isLoading: MutableLiveData<Boolean> = MutableLiveData(false)
+    val isLoading: LiveData<Boolean> = _isLoading
 
+    init {
+        getCourse()
+    }
+
+    fun getCourse() = viewModelScope.launch(Dispatchers.Default) {
+        _isLoading.postValue(true)
+        _isLoading.postValue(false)
+    }
 }
